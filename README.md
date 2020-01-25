d365b33b1474bd54092007fa473eea41a77f5112
mathlun <ratueranato@gmail.com>
2020-01-24 22:00:24 -0300

    corrigindo a documentação & implementando o score no jogo

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Um Simples game feito em python usando pygame.
 - [Random](https://docs.python.org/3/library/random.html): Modulo para valores randomicos
 - [Time](https://docs.python.org/3/library/time.html): Modulo para manipular tempo do game e do objeto.
 
+# Colors Game
+Usando Cores em Hexadecimal
+> - Background: BLACK
+> - Block_COLOR: GREEN
+
+# LOGIC GAME
+> O carro se move para esquerdo e para o lado direito de acordo com suas coordenadas.
+> As Teclas de atalho são:
+> -> : Para o lado direito da tela;
+> <- : Para o lado esquerdo da tela.
--- main.py
+++ main.py
@@ -5,29 +5,23 @@ import pygame
 import time
 import random
 
-# # Import pygame.locais
-# from pygame.locals import (
-#         K_UP,
-#         K_DOWN,
-#         K_LEFT,
-#         K_RIGHT,
-#         K_ESCAPE,
-#         KEYDOWN,
-#         QUIT)
+
 
 # Initialize pygame
 pygame.init()
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 # Define constants for the color object
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLOCK_COLOR = (53,115,255)
 
-# Define constants for the screen width and height
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
+# Define size width the of car
 CAR_WIDTH = 73
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -36,6 +30,11 @@ clock = pygame.time.Clock()
 
 # Image
 carImage = pygame.image.load('car.png')
+
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: " + str(count), True, WHITE)
+    screen.blit(text, (0,0))
 
 # Define Things
 def things(x, y, width, height, color):
@@ -47,15 +46,15 @@ def car(x,y):
 
 # Text Object
 def text_objects(text, font,):
-    textSurface = font.render(text, True, BLACK)
+    textSurface = font.render(text, True, WHITE)
     return textSurface, textSurface.get_rect()
 
 # Message Display
 def message_display(text):
     largeText = pygame.font.Font('SIXTY.TTF', 115)
-    textSurf, TextRect = text_objects(text, largeText)
+    TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
-    screen.blit(textSurf, TextRect)
+    screen.blit(TextSurf, TextRect)
     pygame.display.update()
     time.sleep(2)
     game_loop()
@@ -74,9 +73,13 @@ def game_loop():
     # Draw Thing
     thing_startx = random.randrange(0, SCREEN_WIDTH)
     thing_starty = -600
-    thing_speed = 7
+    thing_speed = 4
     thing_width = 20
     thing_height = 60
+
+    thing_count = 1
+    
+    dodged = 0
 
     gameExit = False
 
@@ -97,11 +100,12 @@ def game_loop():
                     x_change = 0
 
         x += x_change
-        screen.fill(WHITE)
+        screen.fill(BLACK)
         
-        things(thing_startx, thing_starty, thing_width, thing_height, BLACK)
+        things(thing_startx, thing_starty, thing_width, thing_height, WHITE)
         thing_starty += thing_speed
         car(x,y)
+        things_dodged(dodged)
 
         if x > SCREEN_WIDTH - CAR_WIDTH or x < 0:
             crash()
@@ -109,6 +113,9 @@ def game_loop():
         if thing_starty > SCREEN_HEIGHT:
             thing_starty = 0 - thing_height
             thing_startx = random.randrange(0, SCREEN_WIDTH)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
         
         if y < thing_starty + thing_height:
             print('y crossover')
